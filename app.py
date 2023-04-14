5b6729eb3cb79cf03fc6ec94d5062e0520b13bcb
Zele <105638320+ZeleOeO@users.noreply.github.com>
2023-04-14 20:49:54 +0100

    Created

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('index.html', prediction={})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    my_list = request.form.values()
+    ans = model.predict(my_list)
+    diction={}
+    for index, i in enumerate(ans):
+        if i == 0:
+           ans = "BUSINESS NEWS"
+        elif i == 1:
+           ans = "SPORTS NEWS"
+        elif i == 2:
+            ans = "CRIME NEWS"
+        elif i == 3:
+            ans="SCIENCE NEWS"
+        diction[index]=ans
+
+
+    return render_template('index.html',prediction = diction)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,10 @@
+function getText() {
+    const sentence1 = document.getElementById("sentence1").value;
+    // const sentence2 = document.getElementById("sentence2").value;
+    // const sentence3 = document.getElementById("sentence3").value;
+    // const sentence4 = document.getElementById("sentence4").value;
+    // const sentence5 = document.getElementById("sentence5").value;
+    
+    // const sentences = [sentence1, sentence2, sentence3, sentence4, sentence5];
+    return sentence1
+}
